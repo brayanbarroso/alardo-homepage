3e14a438ab898c1f576dca933e9ce6e15907361e
brayanbarroso <brayanbarroso01@gmail.com>
2024-03-26 15:50:31 -0500

    ajustando el logo en modo oscuro

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     const swictherTheme = document.querySelector('.nav__check');
     const sun = document.querySelector('.nav__icon-sun');
     const moon = document.querySelector('.nav__icon-moon');
+    const logo = document.querySelector('.logo');
     const root = document.documentElement;
 
     if(root.getAttribute('data-theme') === 'dark'){
@@ -10,6 +11,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         moon.classList.toggle('active-moon');
         sun.src="./assets/img/Sun_fill_light.svg";
         sun.classList.toggle('active-sun');
+        logo.src="./assets/img/alarado-icon-homepage-light.svg"
     }
 
     swictherTheme.addEventListener('click', toggleTheme);
@@ -18,6 +20,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         // const setTheme = swictherTheme.checked ? 'dark' : 'light';
         if(swictherTheme.checked){
             const setTheme = 'dark';
+            logo.src="./assets/img/alarado-icon-homepage-light.svg"
             // Cambio Imagen Luna
             moon.src="./assets/img/Moon_fill.svg";
             moon.classList.toggle('active-moon')
@@ -29,6 +32,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         }else{
             setTheme = 'light';
             // Cambio Imagen Luna
+            logo.src="./assets/img/alarado-icon-homepage.svg"
             moon.src="./assets/img/Moon_fill_light.svg";
             moon.classList.toggle('active-moon')
             // Cambio Imagen Sol
