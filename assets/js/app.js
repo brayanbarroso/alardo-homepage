200a8cb0c3dd2efff485ec9eb195e93f9b73ef44
brayanbarroso <brayanbarroso01@gmail.com>
2024-04-22 12:00:26 -0500

    detalles menu responsivo

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,12 @@ document.addEventListener('DOMContentLoaded',()=>{
     const sun = document.querySelector('.nav__icon-sun');
     const moon = document.querySelector('.nav__icon-moon');
     const logo = document.querySelector('.logo');
+    
     const root = document.documentElement;
 
     // Menu responsivo
     const hamburguer = document.querySelector('.nav__hamburguer');
+    const icon_hamburguer = document.querySelector('.icon__hamburguer');
     const close = document.querySelector('.nav__close');
     const menu_mobile = document.querySelector('.nav');
 
@@ -16,7 +18,8 @@ document.addEventListener('DOMContentLoaded',()=>{
         moon.classList.toggle('active-moon');
         sun.src="./assets/img/Sun_fill_light.svg";
         sun.classList.toggle('active-sun');
-        logo.src="./assets/img/alarado-icon-homepage-light.svg"
+        logo.src="./assets/img/alarado-icon-homepage-light.svg";
+        icon_hamburguer.src="./assets/img/menu_dark.svg";
     }
 
     swictherTheme.addEventListener('click', toggleTheme);
@@ -34,6 +37,8 @@ document.addEventListener('DOMContentLoaded',()=>{
             sun.classList.toggle('active-sun');
             root.setAttribute('data-theme',setTheme);
             localStorage.setItem('theme',setTheme)
+            //Cambio de color imagen hambuguer
+            icon_hamburguer.src="./assets/img/menu_dark.svg";
         }else{
             setTheme = 'light';
             // Cambio Imagen Luna
@@ -45,15 +50,21 @@ document.addEventListener('DOMContentLoaded',()=>{
             sun.classList.toggle('active-sun');
             root.setAttribute('data-theme',setTheme);
             localStorage.setItem('theme',setTheme)
+            //Cambio de color imagen hambuguer
+            icon_hamburguer.src="./assets/img/menu.svg";
         }
         
     }
 
     hamburguer.addEventListener('click', ()=>{
-        menu_mobile.classList.toggle('nav_menu')
+        // menu_mobile.classList.toggle('nav_menu');
+        menu_mobile.classList.remove('nav');
+        menu_mobile.classList.add('nav_menu');
+        
     })
 
     close.addEventListener('click', ()=>{
         menu_mobile.classList.remove('nav_menu')
+        menu_mobile.classList.add('nav');
     })
 })
